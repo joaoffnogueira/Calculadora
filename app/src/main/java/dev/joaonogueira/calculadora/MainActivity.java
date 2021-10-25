3f9e6fde14c423d1bac0e3073a6e794ca4c2d83d
joaoffnogueira <50890356+joaoffnogueira@users.noreply.github.com>
2021-10-25 10:27:05 -0300

    logic implementation

--- app/src/main/java/dev/joaonogueira/calculadora/MainActivity.java
+++ app/src/main/java/dev/joaonogueira/calculadora/MainActivity.java
@@ -1,14 +1,154 @@
 package dev.joaonogueira.calculadora;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.fathzer.soft.javaluator.DoubleEvaluator;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText e1,e2;
+    private int count=0;
+    private String expression="";
+    private String text="";
+    private Double result=0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        e1=(EditText)findViewById(R.id.editText1);
+        e2=(EditText)findViewById(R.id.editText2);
+
+        e2.setText("0");
+    }
+
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.num0:
+                e2.setText(e2.getText()+"0");
+                break;
+
+            case R.id.num1:
+                e2.setText(e2.getText()+"1");
+                break;
+
+            case R.id.num2:
+                e2.setText(e2.getText()+"2");
+                break;
+
+            case R.id.num3:
+                e2.setText(e2.getText()+"3");
+                break;
+
+
+            case R.id.num4:
+                e2.setText(e2.getText()+"4");
+                break;
+
+            case R.id.num5:
+                e2.setText(e2.getText()+"5");
+                break;
+
+            case R.id.num6:
+                e2.setText(e2.getText()+"6");
+                break;
+
+            case R.id.num7:
+                e2.setText(e2.getText()+"7");
+                break;
+
+            case R.id.num8:
+                e2.setText(e2.getText()+"8");
+                break;
+
+            case R.id.num9:
+                e2.setText(e2.getText()+"9");
+                break;
+
+            case R.id.ponto:
+                if(count==0 && e2.length()!=0)
+                {
+                    e2.setText(e2.getText()+".");
+                    count++;
+                }
+                break;
+
+            case R.id.ca:
+                e1.setText("");
+                e2.setText("");
+                count=0;
+                expression="";
+                break;
+
+            case R.id.somar:
+                operationClicked("+");
+                break;
+
+            case R.id.subtrair:
+                operationClicked("-");
+                break;
+
+            case R.id.dividir:
+                operationClicked("/");
+                break;
+
+            case R.id.multiplicar:
+                operationClicked("*");
+                break;
+
+            case R.id.resultado:
+                /*for more knowledge on DoubleEvaluator and its tutorial go to the below link
+                http://javaluator.sourceforge.net/en/home/*/
+                if(e2.length()!=0)
+                {
+                    text=e2.getText().toString();
+                    expression=e1.getText().toString()+text;
+                }
+                e1.setText("");
+                if(expression.length()==0)
+                    expression="0.0";
+                DoubleEvaluator evaluator = new DoubleEvaluator();
+                try
+                {
+                    //evaluate the expression
+                    result=new DoubleEvaluator().evaluate(expression);
+                    e2.setText(result+"");
+                }
+                catch (Exception e)
+                {
+                    e2.setText("Invalid Expression");
+                    e1.setText("");
+                    expression="";
+                    e.printStackTrace();
+                }
+                break;
+
+        }
+    }
+
+    private void operationClicked(String op)
+    {
+        if(e2.length()!=0)
+        {
+            String text=e2.getText().toString();
+            e1.setText(e1.getText() + text+op);
+            e2.setText("");
+            count=0;
+        }
+        else
+        {
+            String text=e1.getText().toString();
+            if(text.length()>0)
+            {
+                String newText=text.substring(0,text.length()-1)+op;
+                e1.setText(newText);
+            }
+        }
     }
 }
